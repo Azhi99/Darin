76eaa4c93cd67f69e7d28b26110a32a53cf4af52
Azhi99 <azhijabar.aj@gmail.com>
2021-12-18 22:16:55 +0300

    c

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -148,7 +148,7 @@ export default (ctx, inject) => {
   const runtimeConfig = ctx.$config && ctx.$config.axios || {}
   // baseURL
   const baseURL = process.browser
-    ? (runtimeConfig.browserBaseURL || runtimeConfig.browserBaseUrl || runtimeConfig.baseURL || runtimeConfig.baseUrl || 'http://localhost:3000/')
+    ? (runtimeConfig.browserBaseURL || runtimeConfig.browserBaseUrl || runtimeConfig.baseURL || runtimeConfig.baseUrl || 'https://daringame.com/')
       : (runtimeConfig.baseURL || runtimeConfig.baseUrl || process.env._AXIOS_BASE_URL_ || 'https://daringame.com/')
 
   // Create fresh objects for all default header scopes
